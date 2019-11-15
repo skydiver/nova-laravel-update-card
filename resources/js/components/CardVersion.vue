81acbf8ecb370f96267429ed5fab697e80f4f359
Martin M <skydiver@users.noreply.github.com>
2019-11-15 19:03:41 -0300

    Release v1.2.0 (#10)
    * card ui enhancements
    
    * card ui enhancements

---
+++ resources/js/components/CardVersion.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex mt-2 px-3 items-center" :class="cssClass">
+    <span class="mr-1">{{ label }}</span>
+    <strong>{{ version}}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    version: {
+      type: String,
+      default: ""
+    },
+    fullWidth: {
+      type: Boolean,
+      default: false
+    },
+    alignment: {
+      type: String,
+      default: "left"
+    }
+  },
+  computed: {
+    cssClass() {
+      return `${this.cardWidth} ${this.boxAlignment}`;
+    },
+    cardWidth() {
+      if (this.fullWidth) {
+        return "w-full";
+      }
+      return "w-1/2";
+    },
+    boxAlignment() {
+      if (this.fullWidth) {
+        return "justify-center";
+      }
+      if (this.alignment === "right") {
+        return "justify-end";
+      }
+      return "justify-start";
+    }
+  }
+};
+</script>
